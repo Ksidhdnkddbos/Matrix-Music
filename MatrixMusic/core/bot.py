6fc7122758cab62a2eabbd6287ba8da09a3870a7
Ahmed programmer <119659334+DevMatrixArabic125901@users.noreply.github.com>
2023-12-31 16:48:12 +0300

    Matrix ~ Music

--- MatrixMusic/core/bot.py
+++ MatrixMusic/core/bot.py
@@ -12,7 +12,6 @@ class Zelzaly(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            max_concurrent_transmissions=7,
         )
 
     async def start(self):
