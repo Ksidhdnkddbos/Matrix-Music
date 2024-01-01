0b593739fb1d6f82e3780fbb2ab031dae9431f48
Ahmed programmer <119659334+DevMatrixArabic125901@users.noreply.github.com>
2024-01-01 17:14:44 +0300

    Matrix ~ Music

--- MatrixMusic/plugins/play/start-end.py
+++ MatrixMusic/plugins/play/start-end.py
@@ -12,11 +12,11 @@ async def brah2(client, message):
        await message.reply("↯︙تم ايقاف ↫ ⦗ المحادثة المرئية ⦘")
 @app.on_message(filters.video_chat_members_invited)
 async def fuckoff(client, message):
-           text = f"↯︙قام الشخص ↬ ⦗ {message.from_user.mention} ⦘"
+           text = f"↯︙قام الشخص ↫ ⦗ {message.from_user.mention} ⦘"
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
-               text += f"\n↯︙بدعوة شخص الى ⦗ المحادثة المرئية ⦘ ↬ ⦗ {message.from_user.mention} ⦘"
+               text += f"\n↯︙بدعوة شخص الى ⦗ المحادثة المرئية ⦘ ↫ ⦗ {message.from_user.mention} ⦘"
                x += 1
              except Exception:
                pass
