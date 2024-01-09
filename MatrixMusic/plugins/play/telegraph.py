0a241e0c96c614b0c8b3937b993e771cbfd4d6e3
Ahmed programmer <119659334+DevMatrixArabic125901@users.noreply.github.com>
2024-01-09 23:32:24 +0300

    Matrix ~ Music

--- MatrixMusic/plugins/play/telegraph.py
+++ MatrixMusic/plugins/play/telegraph.py
@@ -8,7 +8,7 @@ from MatrixMusic import app
 @app.on_message(command(["تليجراف ميديا","ميديا","تلغراف ميديا","تليكراف ميديا"]))
 async def get_link_group(client, message):
     try:
-        text = await message.reply("Processing...")
+        text = await message.reply("↯︙يعالج ...")
         async def progress(current, total):
             await text.edit_text(f"↯︙يتم رفع الوسائط ↬ ⦗ {current * 100 / total:.1f}% ⦘")
         try:
@@ -16,10 +16,10 @@ async def get_link_group(client, message):
             local_path = await message.reply_to_message.download(location, progress=progress)
             await text.edit_text("↯︙يتم جلب الرابط")
             upload_path = upload_file(local_path) 
-            await text.edit_text(f"**↯︙تم تحويل صورة الى تليجراف ميديا**⤈\n\n⦗ <code>https://telegra.ph{upload_path[0]}</code> ⦘")     
+            await text.edit_text(f"↯︙تم تحويل صورة الى تليجراف ميديا ⤈\n\n⦗ <code>https://telegra.ph{upload_path[0]}</code> ⦘")     
             os.remove(local_path) 
         except Exception as e:
-            await text.edit_text(f"**↯︙File upload failed**\n\n<i>**Reason**: {e}</i>")
+            await text.edit_text(f"↯︙File upload failed\n\n<i>Reason: {e}</i>")
             os.remove(local_path) 
             return         
     except Exception:
