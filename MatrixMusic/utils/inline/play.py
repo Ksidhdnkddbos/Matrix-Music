da02bd4c31c6511dfa91813d2f7234f8925f3cde
Ahmed programmer <119659334+DevMatrixArabic125901@users.noreply.github.com>
2024-01-10 09:06:49 +0300

    Update play.py

--- MatrixMusic/utils/inline/play.py
+++ MatrixMusic/utils/inline/play.py
@@ -76,11 +76,55 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‹ تشغيل ›", callback_data=f"ADMIN Resume|{chat_id}"
+            ), 
+                    ],
+        [
+            InlineKeyboardButton(text="‹ توقف ›", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ إعادة التشغيل ›", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ تخطي ›", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ ايقاف ›", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ كتم ›", callback_data=f"ADMIN Mute|{chat_id}"                       
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ الغاء الكتم ›",callback_data=f"ADMIN Unmute|{chat_id}"
+            ),
+                    ],
+        [
+
+            InlineKeyboardButton(text="‹ خلط ›",callback_data=f"ADMIN Shuffle|{chat_id}",
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ تكرار ›", callback_data=f"ADMIN Loop|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="‹ يمين ›",callback_data=f"Pages Back|1|{videoid}|{chat_id}",
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ رجوع ›",callback_data=f"MainMarkup {videoid}|{chat_id}",
+            ),
+                    ],
+        [
+            InlineKeyboardButton(text="‹ يسار ›",callback_data=f"Pages Forw|1|{videoid}|{chat_id}",
+            ),
+        ],
+    ]
         ],
         [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/XMATTMX")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -161,42 +205,6 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
     return buttons
 
 
-def panel_markup_2(_, videoid, chat_id):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="‹ كتم ›", callback_data=f"ADMIN Mute|{chat_id}"
-            ),
-            InlineKeyboardButton(
-                text="‹ الغاء الكتم ›",
-                callback_data=f"ADMIN Unmute|{chat_id}",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="‹ خلط ›",
-                callback_data=f"ADMIN Shuffle|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="‹ تكرار ›", callback_data=f"ADMIN Loop|{chat_id}"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="◀️",
-                callback_data=f"Pages Back|1|{videoid}|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="‹ رجوع ›",
-                callback_data=f"MainMarkup {videoid}|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="▶️",
-                callback_data=f"Pages Forw|1|{videoid}|{chat_id}",
-            ),
-        ],
-    ]
-    return buttons
 
 def panel_markup_3(_, videoid, chat_id):
     buttons = [
