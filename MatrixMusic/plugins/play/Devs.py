216332253fef777c568515d155b1de9ea7fa2b34
Ahmed programmer <119659334+DevMatrixArabic125901@users.noreply.github.com>
2024-01-24 13:11:52 +0300

    Matrix ~ Music

--- MatrixMusic/plugins/play/Devs.py
+++ MatrixMusic/plugins/play/Devs.py
@@ -101,16 +101,17 @@ async def huhh(client: Client, message: Message):
 
 @app.on_message(command(["تخ"]) & filters.group)
 async def huhh(client, message):
-await message.reply_animation(
-animation=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
-caption=f"""↯︙قتل ↫ {message.from_user.mention}\nالضحيه دا 😢 ↫ {message.from_id.first_name}\nانا لله وانـا اليـه راجعـون 😢😢""",
-)
-reply_markup=InlineKeyboardMarkup(
-[
-[
-InlineKeyboardButton("المقتول 🔪", url=f"{message.from_id.first_name}"),
-],[
-InlineKeyboardButton("‹ : 𝖬𝖺𝖳𝗋𝗂x 𝖳𝖾𝖠𝗆 : ›", url=f"https://t.me/XMATTMX"),
-],
-]
-),
+    await message.reply_animation(
+        animation=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
+        caption=f"""↯︙قتل ↫ {message.from_user.mention}\nالضحيه دا 😢 ↫ {message.from_id.first_name}\nانا لله وانـا اليـه راجعـون 😢😢""",
+    )
+    reply_markup=InlineKeyboardMarkup(
+
+       [
+           [
+               InlineKeyboardButton("المقتول 🔪", url=f"{message.from_id.first_name}"),
+           ],[
+               InlineKeyboardButton("‹ : 𝖬𝖺𝖳𝗋𝗂x 𝖳𝖾𝖠𝗆 : ›", url=f"https://t.me/XMATTMX"),
+           ],
+       ]
+    ),
