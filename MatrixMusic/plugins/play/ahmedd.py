3cc2ad21c0c7a1b3fc516aff0d80b53c591a5473
Ahmed programmer <119659334+DevMatrixArabic125901@users.noreply.github.com>
2024-01-24 11:28:32 +0300

    Matrix ~ Music

--- MatrixMusic/plugins/play/ahmedd.py
+++ MatrixMusic/plugins/play/ahmedd.py
@@ -3,13 +3,13 @@ from MatrixMusic import app
 import asyncio
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from MatrixMusic.core.call import Anon
+from MatrixMusic.core.call import Zelzaly
 from MatrixMusic.utils.database import *
 from pytgcalls.exceptions import NoActiveGroupCall, TelegramServerError
-
+#HHC3C4
 @app.on_message(filters.regex("اسم المساعد"))
 async def tom_name(client, message):
-    assistant = await group_assistant(Anon, message.chat.id)
+    assistant = await group_assistant(Zelzaly, message.chat.id)
     await message.reply("ارسل اسم المساعد الجديد:")
     try:
         new_name = await client.ask(message.chat.id, "اكتب اسم المساعد الجديد:")
